2bb00df36edbc0f5b6f3dd14c0f312789868eee0
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-07-23 17:45:33 -0500

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import BasicRouting from './components/BasicRouting'
+
+import { BrowserRouter } from "react-router-dom"
+
 export default function App() {
   return (
-    <div>Hello from App!</div>
+    <BrowserRouter>
+        <BasicRouting />
+    </BrowserRouter>
   )
 }
